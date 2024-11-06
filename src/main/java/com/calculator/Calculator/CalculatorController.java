9c817f0f69cbe2f6a785f2c32c2fd41d671541f8
Saner Eraslan <sanereraslan2@gmail.com>
2024-11-06 20:54:52 +0300

    toplama uç noktası yazıldı

--- src/main/java/com/calculator/Calculator/CalculatorController.java
+++ src/main/java/com/calculator/Calculator/CalculatorController.java
@@ -1,4 +1,21 @@
 package com.calculator.Calculator;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculate")
 public class CalculatorController {
+
+    @GetMapping("/collect")
+    public int sum(@RequestParam int v1, @RequestParam int v2){
+
+        return  v1+ v2;
+
+    }
+
+
 }
