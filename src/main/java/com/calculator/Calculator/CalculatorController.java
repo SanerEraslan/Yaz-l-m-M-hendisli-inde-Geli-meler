f6b82d808b7ae0f0648bb9e933cde1e33bf20a89
Saner Eraslan <sanereraslan2@gmail.com>
2024-11-06 20:59:19 +0300

    çarpma uç noktası yazıldı

--- src/main/java/com/calculator/Calculator/CalculatorController.java
+++ src/main/java/com/calculator/Calculator/CalculatorController.java
@@ -1,21 +1,24 @@
 package com.calculator.Calculator;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculate")
 public class CalculatorController {
 
     @GetMapping("/collect")
-    public int sum(@RequestParam int v1, @RequestParam int v2){
+    public int collect(@RequestParam int v1, @RequestParam int v2){
 
         return  v1+ v2;
 
     }
 
+    @PostMapping("multiply")
+    public int multiply(@RequestParam int v1 ,@RequestParam int v2){
+
+        return v1*v2;
+
+    }
 
 }
